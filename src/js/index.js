2d09f474dbf3639119f9ace0795ec69ea058cc36
Sergey <Segas95@yandex.ru>
2023-03-20 12:45:37 +0300

    gggg

--- src/js/index.js
+++ src/js/index.js
@@ -1,76 +1,100 @@
-let year = new Date().getFullYear();
+// const element = document.querySelector(".step-3");
 
-const footerCurrentYear = (document.querySelector(
-  ".footer__current-year"
-).textContent = `${year} год`);
+// element.scrollIntoView(false);
 
-function disabledScroll() {
-  document.body.style.cssText = ` overflow: hidden`;
-}
-const target = document.querySelector("#how-we-work");
-function horizontalSkroll() {
-  window.addEventListener("wheel", function (e) {
-    if (e.deltaY > 0 && blockScroll == true) {
-      target.scrollLeft += 700;
-      console.log(target.scrollLeft + " Left");
-    } else if (e.deltaY < 0 && blockScroll == true) {
-      target.scrollLeft -= 700;
-      console.log(target.scrollLeft + "Left");
-    }
-  });
-}
+// window.addEventListener("wheel", function (e) {
 
-const options = {
-  threshold: 0.5,
-  rootMargin: "0px 0px -48% 0px",
-};
+// })
 
-const target2 = document.querySelector(".step-4");
-const callback = function (entries, observer) {
-  entries.forEach((entry) => {
-    const {isIntersecting} = entry;
-    let blockScroll = false;
-    console.log(isIntersecting + " is");
-    direction = "";
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
-    if (isIntersecting) {
-      blockScroll = true;
-      disabledScroll();
-      console.log(blockScroll + "Block srooll srabotal");
+// function disabledScroll() {
+//   document.body.style.cssText = ` overflow: hidden`;
+// }
+// const target = document.querySelector("#how-we-work");
+// function horizontalSkroll() {
+//   window.addEventListener("wheel", function (e) {
+//     if (e.deltaY > 0 && blockScroll == true) {
+//       target.scrollLeft += 700;
+//       console.log(target.scrollLeft + " Left");
+//     } else if (e.deltaY < 0 && blockScroll == true) {
+//       target.scrollLeft -= 700;
+//       console.log(target.scrollLeft + "Left");
+//     }
+//   });
+// }
 
-      window.addEventListener("wheel", (e) => {
-        if (e.deltaY < 0) {
-          if (direction !== "up" || target.scrollLeft > 10) {
-            console.log("up");
-            target.scrollLeft -= 700;
-            console.log(target.scrollLeft);
-            target.scrollLeft < 10
-              ? (document.body.style.cssText = ` overflow:scroll`)
-              : null;
-            direction = "up";
-          }
-        }
-        if (e.deltaY > 0) {
-          if (direction !== "down" || target.scrollLeft < 2080) {
-            console.log("down");
-            target.scrollLeft += 700;
-            target.scrollLeft > 2080
-              ? (document.body.style.cssText = ` overflow:scroll`)
-              : null;
-            direction = "down";
-          }
-        }
-      });
-    } else {
-      blockScroll = false;
-    }
-    console.log(blockScroll);
-  });
-};
+// const options = {
+//   threshold: 0.5,
+//   rootMargin: "0px 0px -48% 0px",
+// };
 
-const observer = new IntersectionObserver(callback, options);
+// const target2 = document.querySelector(".step-4");
+// const callback = function (entries, observer) {
+//   entries.forEach((entry) => {
+//     const {isIntersecting} = entry;
+//     let blockScroll = false;
+//     console.log(isIntersecting + " is");
+//     direction = "";
 
-observer.observe(target);
+//     if (isIntersecting) {
+//       blockScroll = true;
+//       disabledScroll();
+//       console.log(blockScroll + "Block srooll srabotal");
+
+//       window.addEventListener("wheel", (e) => {
+//         if (e.deltaY < 0) {
+//           if (direction !== "up" || target.scrollLeft > 10) {
+//             console.log("up");
+//             target.scrollLeft -= 700;
+//             console.log(target.scrollLeft);
+//             target.scrollLeft < 10
+//               ? (document.body.style.cssText = ` overflow:scroll`)
+//               : null;
+//             direction = "up";
+//           }
+//         }
+//         if (e.deltaY > 0) {
+//           if (direction !== "down" || target.scrollLeft < 2080) {
+//             console.log("down");
+//             target.scrollLeft += 700;
+//             target.scrollLeft > 2080
+//               ? (document.body.style.cssText = ` overflow:scroll`)
+//               : null;
+//             direction = "down";
+//           }
+//         }
+//       });
+//     } else {
+//       blockScroll = false;
+//     }
+//     console.log(blockScroll);
+//   });
+//};
+
+// const observer = new IntersectionObserver(callback, options);
+
+// observer.observe(target);
 
 const addButton = document.querySelector(".selected-cases__add");
 const bicycleCity = document.querySelector(".selected-cases__bicycle-city");
@@ -88,4 +112,11 @@ addButton.addEventListener("click", () => {
   glonass.style.display = "inherit";
   document.querySelector(".selected-cases__add-text-h3").textContent =
     "Нажмите, чтобы загрузить еще 1 работу";
+  addButton.style.margin = "20px 0 0 0";
 });
+
+let year = new Date().getFullYear();
+
+const footerCurrentYear = (document.querySelector(
+  ".footer__current-year"
+).textContent = `${year} год`);
